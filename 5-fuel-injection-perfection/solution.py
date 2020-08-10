625e66c256462e7c1af67a56b41e227fe88381fa
Mike <mnguyen287@ucmerced.edu>
2020-08-10 13:48:18 -0700

    initial commit

---
+++ 5-fuel-injection-perfection/solution.py
@@ -0,0 +1,12 @@
+def solution(n):
+  count = 0
+  fuel = int(n)
+  while (fuel > 1): 
+    if (fuel % 2 == 0):
+      fuel = fuel / 2
+    elif ((fuel == 3) or (fuel % 4 == 1)):
+      fuel -= 1
+    else:
+      fuel += 1
+    count += 1
+  return count
